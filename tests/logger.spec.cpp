2b5add1a2fa511610f449372e1bc91545e05175f
David Delassus <david.jose.delassus@gmail.com>
2024-04-04 14:18:38 +0200

    :white_check_mark: add test case for printer_trait

--- tests/logger.spec.cpp
+++ tests/logger.spec.cpp
@@ -23,3 +23,22 @@ TEST_CASE("it should format log records with logfmt format") {
     logfmtxx::field{"key2", "value2"}
   );
 }
+
+TEST_CASE("it should work with functors") {
+  struct printer {
+    void operator()(const std::string& message) {
+      CHECK(message == "time=2001-01-01T00:00:00Z level=info message=\"hello\" key1=\"value1\" key2=\"value2\"");
+    }
+  };
+
+  auto logger = logfmtxx::logger<dummy_clock>{
+    printer{},
+    logfmtxx::field{"key1", "value1"}
+  };
+
+  logger.log(
+    logfmtxx::level::info,
+    "hello",
+    logfmtxx::field{"key2", "value2"}
+  );
+}
