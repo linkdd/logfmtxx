3113961b8e0afefc94f909315301b45fe15219e4
David Delassus <david.jose.delassus@gmail.com>
2025-03-10 23:45:08 +0100

    :rotating_light: `auto i` is implicitly an int instead of size_t

--- include/logfmtxx.hpp
+++ include/logfmtxx.hpp
@@ -156,12 +156,12 @@ namespace logfmtxx {
         stream << "level=" << details::serialize(record.lvl) << " ";
         stream << "message=" << std::quoted(record.msg);
 
-        for (auto i = 0; i < record.global_ctx_size; ++i) {
+        for (std::size_t i = 0; i < record.global_ctx_size; ++i) {
           const auto& [key, value] = record.global_ctx[i];
           stream << " " << key << "=" << value;
         }
 
-        for (auto i = 0; i < record.local_ctx_size; ++i) {
+        for (std::size_t i = 0; i < record.local_ctx_size; ++i) {
           const auto& [key, value] = record.local_ctx[i];
           stream << " " << key << "=" << value;
         }
